6d9bd6a7fbf294f8f2e80c63c80ef96838b54d70
crubio <crubio@avanzalia.es>
2019-11-08 12:55:02 +0100

    Updated __init__ to include OpenFoamCase

--- AutoStudies/__init__.py
+++ AutoStudies/__init__.py
@@ -4,10 +4,15 @@ from AutoStudies.Path import *
 
 # Import cases
 from AutoStudies.Cases.SolsticeCase import SolsticeCase
+
 try:
     from AutoStudies.Cases.ExcelCase import ExcelCase
 except ImportError:
     pass
 
+try:
+    from AutoStudies.Cases.OpenFoamCase import OpenFoamCase
+except ImportError:
+    pass
 
 
