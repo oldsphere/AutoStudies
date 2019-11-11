8ce2a2972f9400b67df75d87515245d1177a8616
crubio <crubio@avanzalia.es>
2019-11-11 17:13:38 +0100

    Added OpenFoamCase set_name() method

--- AutoStudies/Cases/OpenFoamCase.py
+++ AutoStudies/Cases/OpenFoamCase.py
@@ -16,40 +16,47 @@ class OpenFoamCase(AbstractCase):
 
         self.__dict__.update(
             {
-            'set_snappyHexMesh' : partial(self.set_dictSettings, dictName='snappyHexMeshDict'),
-            'set_fvSchemes' : partial(self.set_dictSettings, dictName='fvSchemes'),
-            'set_fvSolution' : partial(self.set_dictSettings, dictName='fvSolution'),
+            'set_snappyHexMesh' : partial(self.set_dictSettings, 'snappyHexMeshDict'),
+            'set_fvSchemes' : partial(self.set_dictSettings, 'fvSchemes'),
+            'set_fvSolution' : partial(self.set_dictSettings, 'fvSolution'),
             }
         )
 
     def set_name(self, name):
         ''' handle the case renaming '''
-        pass
+        new_path = os.path.join( os.path.dirname(self._path), name )
+        os.rename(self._path, new_path)
+        self._case = SolutionDirectory(new_path)
+        self._path = new_path
+        self.name = name
 
     def run(self):
         pass
 
-    def runApp(self, app, args={}):
+    def runApp(self, app, args={}, quiet=False):
         ''' Run an application with the specified arguments '''
+
+        quiet_options = {
+            'silent' : True if quiet else False,
+        }
 
         cmd = [app, '-case', self._path]
         cmd += sum([['-{}'.format(k),v] for k,v in args.items()], [])
-        return UtilityRunner(cmd).start()
+        return UtilityRunner(cmd, **quiet_options).start()
 
     def clone(self, newName=None):
         ''' Creates a clone of current case '''
-
         if not newName: newName = self._path + '_clone'
         cloned_case = self._case.cloneCase(newName)
         ncase = self.__class__(cloned_case.name)
         return ncase
 
-
     def set_dictSettings(self, dictName, setting, value):
         ''' Change a dictionary value '''
         f = self.findFile(dictName)
         if not f:
-            raise OSError('no "{}" found in the case {}'.format(dictName, self.name))
+            raise OSError('no "{}" found in the case {}'
+                          .format(dictName, self.name))
         if len(f) > 1:
             raise OSError('multiple "{}" found!'.format(dictName))
 
@@ -69,7 +76,7 @@ class OpenFoamDictionary(ParsedParameterFile):
 
     def __init__(self, filepath):
         super().__init__(filepath)
-        self.name = os.path.basename(self.name)
+        self.basename = os.path.basename(self.name)
 
     @staticmethod
     def _set(foamDict, parameter, value):
@@ -83,13 +90,17 @@ class OpenFoamDictionary(ParsedParameterFile):
             )
 
         if not parameter in foamDict.keys():
-            raise Exception(
-                'There is not parameter {} in {}' \
-                .format(parameter, foamDict.name)
-            )
+            return -1
 
         foamDict[parameter] = value
 
     def set_parameter(self, parameter, value):
-        return self._set(self.content, parameter, value)
+
+        out = self._set(self.content, parameter, value)
+        if out == -1:
+            raise Exception(
+                'There is not parameter {} in {}' \
+                .format(parameter, self.basename)
+            )
+        return out
 
