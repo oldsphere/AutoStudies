fa9261c9cee0da6c8656a596e96300af675ce91e
crubio <crubio@avanzalia.es>
2019-11-11 12:50:46 +0100

    Updated version of setup

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.6",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Topic :: Utilities"
     ],
